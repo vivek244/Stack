122990e5f00b0e6f40d446a03d434d70797a8f4f
Vivek <Vivek@DESKTOP-7L33ALP>
2021-04-07 15:07:32 +0530

    ok

--- Java/src/Kstack.java
+++ Java/src/Kstack.java
@@ -20,10 +20,46 @@ public class Kstack {
 		return (free == -1);
 	}
 	
+	void push(int item,int sn) {
+		if (isfull()) {
+			System.out.println("Overflow");
+			return ;
+		}
+		int i=free;
+		free=next[i];
+		next[i]=top[sn];
+		top[sn]= i;
+		arr[i]=item;
+	}
+	
+	int pop(int sn) {
+		if (isEmpty(sn)) {
+			System.out.println("Underflow");
+			return Integer.MAX_VALUE;
+		}
+		int i=top[sn];
+		top[sn]= next[i];
+		next[i]=free;
+		free=i;
+		return arr[i];
+	}
+	
+	 boolean isEmpty(int sn) 
+     {
+         return (top[sn] == -1);
+     }
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Kstack k=new Kstack(3, 10);
+		k.push(14, 2);
+		k.push(12, 1);
+		k.push(20, 0);
+		k.push(10, 0);
+		k.push(18, 1);
+		System.out.println("Popped element from stack 2 is " + k.pop(2));
+        System.out.println("Popped element from stack 1 is " + k.pop(1));
+        System.out.println("Popped element from stack 0 is " + k.pop(0));
 
 	}
 
